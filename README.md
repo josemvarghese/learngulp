57842509c0b00e32f56aade04c4acaada5d01ce2
jose <jose@voris.co.in>
2017-03-25 14:32:47 +0530

    all task executing in one go

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # learngulp
 its for learning gulp
+
+
+npm install gulp 
+
+for installing gulp
+
+
+npm install --save-dev gulp-uglify
+
+
+for removing log messages and minimising js files by removing spaces 
+
+
+we can write multipile task for each function
+
+
+npm install --save gulp-uglifycss
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
-var gulp = require('gulp'),uglify = require('gulp-uglify');
+var gulp = require('gulp'),uglify = require('gulp-uglify'),uglifycss = require('gulp-uglifycss');
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
   // place code for your default task here
   gulp.src('static/dashboard/scripts/*.js')
   .pipe(uglify())
   .pipe(gulp.dest('static/minjs'));
   console.log("gulp working");
 });
+
+gulp.task('jprogress', function() {
+  // place code for your default task here
+  gulp.src('static/dashboard/jprogress/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('static/jprogress'));
+  console.log("gulp working");
+});
+
+gulp.task('css', function () {
+  gulp.src('static/dashboard/css/*.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('static/css'));
+});
+
+gulp.task('compress',['scripts','jprogress','css']);
